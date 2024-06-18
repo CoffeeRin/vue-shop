43dcb2d9c0b08221907015ceffbc74ef9efcdf55
Coffee.Lin <2080205047@qq.com>
2024-06-18 14:26:31 +0800

    elementPlus自动按需引入配置

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,26 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+//Elementplus按需导入的自动导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    //配置插件
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
+    // 实际的路径转换 @ -> src
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
