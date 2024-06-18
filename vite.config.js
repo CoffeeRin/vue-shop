b94a80a86dd9bac719f88aeb046f296aa6eb4d4f
CoffeeLin <2080205047@qq.com>
2024-06-18 16:55:09 +0800

    elementPlus主题色定制

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-//Elementplus按需导入的自动导入
+//Elementplus自动按需引入
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -18,13 +18,26 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        //1.配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({ importStyle: "sass" }),
+      ],
     }),
   ],
   resolve: {
     // 实际的路径转换 @ -> src
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2.自动导入定制化样式文件进行样式覆盖掉原来的主题色
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
