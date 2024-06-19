dad189c99f56e4f86d49ec667ac9a6c9e3ef3391
CoffeeLin <2080205047@qq.com>
2024-06-20 00:56:52 +0800

    配置二级分类路由

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -10,7 +10,7 @@ import { useCategory } from './composables/useCategory'
 const { bannerList } = useBanner()
 
 //获取分类数据
-const {categoryData} = useCategory()
+const { categoryData } = useCategory()
 </script>
 
 <template>
@@ -35,7 +35,7 @@ const {categoryData} = useCategory()
         <h3>全部分类</h3>
         <ul>
           <li v-for="i in categoryData.children" :key="i.id">
-            <RouterLink to="/">
+            <RouterLink :to="`/category/sub/${i.id}`">
               <img :src="i.picture" />
               <p>{{ i.name }}</p>
             </RouterLink>
