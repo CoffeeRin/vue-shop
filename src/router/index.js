62f102bf21da58602c2dbcb8cc419fc68d0ee9ea
CoffeeLin <2080205047@qq.com>
2024-06-20 23:47:28 +0800

    详情页及其路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -7,37 +7,42 @@ import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
+import Detail from '@/views/Detail/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path:'/',
-      component:Layout,
-      children:[
+      path: '/',
+      component: Layout,
+      children: [
         {
-          path:'', // 在首页时也能渲染，或者用重定向
-          component:Home
+          path: '', // 在首页时也能渲染，或者用重定向
+          component: Home
         },
         {
-          path:'category/:id',
-          component:Category
+          path: 'category/:id',
+          component: Category
         },
         {
-          path:'category/sub/:id',
-          component:SubCategory
+          path: 'category/sub/:id',
+          component: SubCategory
+        },
+        {
+          path: 'detail/:id',
+          component: Detail
         }
       ]
     },
     {
-      path:'/login',
-      component:Login,
+      path: '/login',
+      component: Login,
     }
   ],
   //路由滚动行为定制
-  scrollBehavior(){
-    return{
-      top:0
+  scrollBehavior() {
+    return {
+      top: 0
     }
   }
 })
